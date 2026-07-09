ccd2eb72d6cbbfe249453850b4a99ab7998ea760
TGTiler <113866367+TGTiler@users.noreply.github.com>
2026-07-09 12:08:40 -0300

    Update certificate validity time handling

--- network/p2p_node.py
+++ network/p2p_node.py
@@ -581,8 +581,8 @@ class P2PNode:
             .issuer_name(issuer)
             .public_key(key.public_key())
             .serial_number(x509.random_serial_number())
-            .not_valid_before(datetime.datetime.utcnow())
-            .not_valid_after(datetime.datetime.utcnow()
+            .not_valid_before(datetime.datetime.now(datetime.timezone.utc))
+            .not_valid_after(datetime.datetime.now(datetime.timezone.utc)
                              + datetime.timedelta(days=3650))
             .add_extension(x509.SubjectAlternativeName([
                 x509.DNSName("localhost"),
